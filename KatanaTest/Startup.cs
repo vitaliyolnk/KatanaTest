69ee2fae63170b7bc7a57301fa8da7b49771d4f1
vitaliyolnk <vitaliyolnk@gmail.com>
2014-12-08 20:18:33 +0000

    Project added

---
+++ KatanaTest/Startup.cs
@@ -0,0 +1,28 @@
+﻿using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KatanaTest
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseErrorPage();
+            
+            app.Run(context =>
+            {
+                if (context.Request.Path.Value == "/fail")
+                {
+                    throw new Exception("Random exception");
+                }
+
+                context.Response.ContentType = "text/plain";
+
+                return context.Response.WriteAsync("Hello World!");
+            });
+        }
+    }
+}
